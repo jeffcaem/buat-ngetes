5cd95345bfc28432614def5eae317ff00b396bf1
jeffcaem <91406368+jeffcaem@users.noreply.github.com>
2022-12-10 16:22:46 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,7 +14,7 @@ global.APIKeys = {
 
 // setting 
 global.autoread = false // auto read message
-global.autobio = true 
+global.autobio = false
 global.userRegister = false 
 global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
 
